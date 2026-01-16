0215fb2c2b9d2e53bfe08722273e27ccb1681492
Marcin <piatekmarcin@proton.me>
2026-01-17 00:07:48 +0100

    Aproksymacja 2 z wykorzystaniem lambd

--- APR2_cw_13/D_25_APR2_Piatek_Marcin.cpp
+++ APR2_cw_13/D_25_APR2_Piatek_Marcin.cpp
@@ -1,174 +1,210 @@
 // Visual Studio Code 1.105.1
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <vector>
+#include <functional>
+#include <iomanip>
 
 using Matrix = std::vector<std::vector<double>>;
 using Vector = std::vector<double>;
 
-// Wyliczanie funkcji g
-double g(double x, u_int j)
-{
-    // Dla j=0: g(x,j) = f(x)
-    if (j == 0)
-        return f(x);
+// Wyliczanie wartości funkcji g
+double g(double x, unsigned j);
 
-    // Dla j>0: g(x) = f(x) * x^i
-    double f_x = f(x);
-    for (int i = 0; i < j; ++i)
+// Wyliczanie wartości funkcji h
+double h(double x, unsigned k, const Vector &p);
+
+// Wyliczanie wielomianu
+double P(double x, unsigned k, const Vector &p);
+
+// Funkcja obliczająca całki oznaczone metodą trapezów oraz zliczająca liczbę kroków
+std::pair<double, int> calkaTrapezow(std::function<double(double)> func, double a, double b, double eps = 1.0e-5); // Przyjmuje jako argument funkcję, aby możliwe było wygodne definiowanie funkcji całkowanej
+
+// Wyliczanie funkcji f
+double f(double x);
+
+// Wartość funkcji błędu aproksymacji
+double S(unsigned k, double a, double b, const Vector &p);
+
+// Całkowanie funkcji g
+double I_j(unsigned j, double a, double b);
+
+// Budowa macierzy układu równań
+std::pair<Matrix, Vector> buildMatrix(unsigned k, double a, double b);
+
+// Rozwiązanie układu równań metodą Gaussa-Jordana
+Vector gaussJordan(Matrix matrix, Vector vector);
+
+int main()
+{
+    std::cout << std::setprecision(20);
+
+    double a{}, b{};
+    std::cout << "\n\nPodaj początek przedziału a: ";
+    std::cin >> a;
+    std::cout << "\nPodaj koniec przedziału b: ";
+    std::cin >> b;
+
+    unsigned k{};
+    std::cout << "\nPodaj stopień wielomianu k: ";
+    std::cin >> k;
+
+    // Budowa układu równań
+    auto [macierz, wektor_I] = buildMatrix(k, a, b);
+
+    // Rozwiązanie układu
+    Vector p = gaussJordan(macierz, wektor_I);
+
+    // Wypisanie współczynników
+    std::cout << "\n\nWspółczynniki wielomianu P_" << k << "(x):\n";
+    for (unsigned j = 0; j <= k; ++j)
     {
-        f_x *= x;
+        std::cout << "\t\tp" << j << " = " << p[j] << "\n";
     }
-    return f_x;
+
+    // Obliczenie błędu aproksymacji
+    double error = S(k, a, b, p);
+    std::cout << "\nBłąd aproksymacji S(" << k << ") = " << error << "\n";
+
+    // Testowanie wartości wielomianu
+    char wybor = 't';
+    while (wybor == 't' || wybor == 'T')
+    {
+        double x;
+        std::cout << "\nPodaj x = ";
+        std::cin >> x;
+
+        std::cout << "P_" << k << "(" << x << ") = " << P(x, k, p) << "\n";
+
+        std::cout << "\nNastępne x? (t/n): ";
+        std::cin >> wybor;
+    }
+
+    return 0;
 }
 
-// Wyliczanie funkcji h
-double h(double x, u_int k, Vector &p)
+double g(double x, unsigned j)
 {
-    double temp = f(x) - P(x, k, p); // Wartość wyrażenia wewnętrznęgo
+    if (j == 0)
+        return f(x);
+    return f(x) * pow(x, j);
+}
 
-    // Wartość funkcji to kwadrat wyrażenia wewnętrznego
-    return temp * temp;
-};
-
-double P(double x, u_int k, Vector &p)
+double P(double x, unsigned k, const Vector &p)
 {
     double sum{0};
 
-    for (int j = 0; j < k; ++j)
-    {
-        // Wyliczanie x^j
-        double x_ToPow_j{x}; // x do potęgi j-tej
-        for (int i = 0; i < j; ++i)
-        {
-            x_ToPow_j *= x;
-        }
-
-        sum += p[j] * x_ToPow_j;
-    }
+    for (int j = 0; j <= k; ++j)
+        sum += p[j] * pow(x, j);
 
     return sum;
 }
 
-// Funkcja obliczająca całki oznaczone metodą trapezów oraz zliczająca liczbę kroków
-std::pair<double, int> calka_h(double a, double b, double x, u_int k, Vector &p, double eps = 1.0e-5)
+double h(double x, unsigned k, const Vector &p)
+{
+    double blad = f(x) - P(x, k, p);
+
+    return blad * blad;
+}
+
+std::pair<double, int> calkaTrapezow(std::function<double(double)> func, double a, double b, double eps)
 {
 
-    int krok{0};            // Numer kroku
-    int m{1};               // Ilość przedziałów
-    double interval{b - a}; // Długość przedziału
+    int krok{0};     // Numer kroku
+    int m{1};        // Ilość przedziałów
+    double h{b - a}; // Długość przedziału
 
-    double f_a{h(a, k, p)}; // Wartość funkcji f_a
-    double f_b{h(b, k, p)}; // Wartość funkcji f_b
+    double f_a{func(a)}; // Wartość funkcji f_a
+    double f_b{func(b)}; // Wartość funkcji f_b
 
-    double T_nowe{(interval / 2.) * (f_a + f_b)};
+    double T_nowe{(h / 2.) * (f_a + f_b)};
     double T_stare{};
 
     do
     {
         m = 2 * m;
         krok++;
-        interval = interval / 2.;
+        h = h / 2.;
 
         // Wyliczanie sumy
         double suma{};
         for (int i = 1; i < m; ++i)
         {
-            suma += f(a + i * interval); // x = a+(i*h); y_i = f(x_i)
+            suma += func(a + i * h); // x = a+(i*h); y_i = f(x_i)
         }
 
         T_stare = T_nowe;
-        T_nowe = interval / 2. * (f_a + f_b + 2.0 * suma);
+        T_nowe = h / 2. * (f_a + f_b + 2.0 * suma);
 
     } while (fabs(T_nowe - T_stare) >= eps);
 
     return std::make_pair(T_nowe, krok);
 }
 
-std::pair<double, int> calka_g(double a, double b, double x, u_int j, double eps = 1.0e-5)
-{
-
-    int krok{0};            // Numer kroku
-    int m{1};               // Ilość przedziałów
-    double interval{b - a}; // Długość przedziału
-
-    double f_a{g(a, j)}; // Wartość funkcji f_a
-    double f_b{g(b, j)}; // Wartość funkcji f_b
-
-    double T_nowe{(interval / 2.) * (f_a + f_b)};
-    double T_stare{};
-
-    do
-    {
-        m = 2 * m;
-        krok++;
-        interval = interval / 2.;
-
-        // Wyliczanie sumy
-        double suma{};
-        for (int i = 1; i < m; ++i)
-        {
-            suma += f(a + i * interval); // x = a+(i*h); y_i = f(x_i)
-        }
-
-        T_stare = T_nowe;
-        T_nowe = interval / 2. * (f_a + f_b + 2.0 * suma);
-
-    } while (fabs(T_nowe - T_stare) >= eps);
-
-    return std::make_pair(T_nowe, krok);
-}
-
-// Wyliczanie funkcji f
 double f(double x)
 {
-    return exp(-x);
+    // return exp(-x);
 
-    /* funkcje testowe
-    return -1+2*x;
-    return 3-2*x+x*x;
-    */
+    // funkcje testowe
+    // return -1 + 2 * x;
+    return 3 - 2 * x + x * x;
 }
 
-// Wartość funkcji błędu aproksymacji
-double S(double a, double b, double x, u_int k, Vector &p)
+double S(unsigned k, double a, double b, const Vector &p)
 {
-    return calka_h(a, b, x, k, p).first;
+    // Lambda funkcji h, aby działało "podkładanie" funkcji pod całkę
+    auto func_h = [k, &p](double x)
+    {
+        return h(x, k, p);
+    };
+
+    return calkaTrapezow(func_h, a, b).first;
 }
 
-double I_j(double a, double b, double x, u_int j)
+double I_j(unsigned j, double a, double b)
 {
-    return calka_g(a, b, x, j).first;
+    // Lambda funkcji g, aby działało "podkładanie" funkcji pod całkę
+    auto func_g = [j](double x)
+    {
+        return g(x, j);
+    };
+
+    return calkaTrapezow(func_g, a, b).first;
 };
 
-Matrix buildAugmentedMtrx(const Vector &x, unsigned m, unsigned n, double a, double b)
+std::pair<Matrix, Vector> buildMatrix(unsigned k, double a, double b)
 {
-    // Tworzenie macierzy kwadratowej (n+1) x (m+1)
-    // Ostatnia kolumna dopisywana jest w funkcji realizującej metodę Gaussa-Jordana
-    Matrix augMtrx(n + 1, Vector(m + 1));
+
+    // Tworzenie macierzy kwadratowej (k+1) x (k+1)
+    Matrix Mtrx(k + 1, Vector(k + 1));
 
     // Wypełnienie macierzy
-    for (int i = 0; i <= m; ++i)
+    for (int row = 0; row <= k; ++row)
     {
-        double a_i{0}, b_i{0};
-        for (int z = 0; z < i; ++z)
+        for (int col = 0; col <= k; ++col)
         {
-            a_i *= a;
-            b_i *= b;
-        }
-
-        for (int j = 0; j <= m; ++j)
-        {
-            double delta_i{b_i - a_i};
-            augMtrx[i][j] = delta_i / n + 1;
+            // Wykładnik i = j+1, ..., j+(k+1)
+            int i{row + col};
+            if (i == 0)
+                Mtrx[row][col] = b - a;
+            else
+                Mtrx[row][col] = (pow(b, i + 1) - pow(a, i + 1)) / (i + 1);
         }
     }
 
-    return augMtrx;
+    // Wektor b macierzy rozszerzonej [A|b]
+    Vector Vec(k + 1);
+
+    // b = I_j = całka z [x^j * f(x)]dx
+    for (unsigned j = 0; j <= k; ++j)
+    {
+        Vec[j] = I_j(j, a, b);
+    }
+
+    return std::make_pair(Mtrx, Vec);
 }
 
-// Funkcja rozwiązująca układ równań metodą eliminacji Gaussa-Jordana
 Vector gaussJordan(Matrix matrix, Vector vector)
 {
     int dimension = matrix.size();
@@ -242,32 +278,4 @@ Vector gaussJordan(Matrix matrix, Vector vector)
     }
 
     return solution;
-}
-
-int main()
-{
-    constexpr double eps{1.0e-5};
-
-    double a{}, b{};
-    std::cout << "\n\nPodaj koniec przedziału a: ";
-    std::cin >> a;
-    std::cout << "\tPodaj b: ";
-    std::cin >> b;
-    u_int k{};
-    std::cout << "\nPodaj stopień k wielomianu aproksymującego P_k: ";
-    std::cin >> k;
-
-    double x{};
-    char wybor = 't';
-    while (wybor == 't' || wybor == 'T')
-    {
-        std::cout << "\nPodaj x = ";
-        std::cin >> x;
-        std::cout << "P(" << x << ") = " << P(x, k, p);
-        << "\n";
-        std::cout << "\nNastepne x? (t/n): ";
-        std::cin >> wybor;
-    }
-
-    return 0;
 }
